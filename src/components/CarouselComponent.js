7b74be725f75de25a7e684356ee993a0040e7a6c
Amrit Kafle <akafle1@toromail.csudh.edu>
2024-05-28 16:18:28 -0700

    reduced image size

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Grid, Card, CardMedia, CardContent, Typography, Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Card, CardMedia, CardContent, Typography, Box, CircularProgress } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
 import pipes from '../assets/pipes3.jpg';
 import vivazen from '../assets/vivazen.JPG';
@@ -12,7 +12,8 @@ function CarouselComponent() {
       name: "Premium Smoking Pipes",
       description: "Discover our range of premium smoking pipes, crafted for the perfect smoking experience. Combining elegance and functionality, our pipes are ideal for both casual and dedicated smokers.",
       image: pipes
-    }, {
+    },
+    {
       name: "Boost Your Performance with Rhino",
       description: "Unlock your full potential with Rhino, the ultimate sex pill for enhanced performance and stamina. Experience heightened sensations and long-lasting effects.",
       image: rhinos
@@ -24,33 +25,60 @@ function CarouselComponent() {
     }
   ];
 
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const preloadImages = () => {
+      const promises = items.map(item => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = item.image;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      });
+
+      Promise.all(promises)
+        .then(() => setImagesLoaded(true))
+        .catch(error => console.error('Error loading images:', error));
+    };
+
+    preloadImages();
+  }, []);
+
   return (
     <Container>
       <Box mt={4}>
-        <Carousel>
-          {items.map((item, i) => (
-            <Grid container spacing={3} key={i} alignItems="center" justifyContent="center">
-              <Grid item xs={12}>
-                <Card className="carousel-card">
-                  <CardMedia
-                    component="img"
-                    image={item.image}
-                    title={item.name}
-                    className="carousel-image"
-                  />
-                  <CardContent className="carousel-content">
-                    <Typography gutterBottom variant="h4" component="div" className="carousel-title">
-                      {item.name}
-                    </Typography>
-                    <Typography variant="body1" component="p" className="carousel-description">
-                      {item.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
+        {imagesLoaded ? (
+          <Carousel>
+            {items.map((item, i) => (
+              <Grid container spacing={3} key={i} alignItems="center" justifyContent="center">
+                <Grid item xs={12}>
+                  <Card className="carousel-card">
+                    <CardMedia
+                      component="img"
+                      image={item.image}
+                      title={item.name}
+                      className="carousel-image"
+                    />
+                    <CardContent className="carousel-content">
+                      <Typography gutterBottom variant="h4" component="div" className="carousel-title">
+                        {item.name}
+                      </Typography>
+                      <Typography variant="body1" component="p" className="carousel-description">
+                        {item.description}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
               </Grid>
-            </Grid>
-          ))}
-        </Carousel>
+            ))}
+          </Carousel>
+        ) : (
+          <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+            <CircularProgress />
+          </Box>
+        )}
       </Box>
     </Container>
   );
